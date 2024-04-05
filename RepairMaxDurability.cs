fd1d83957908a0138983002aa5b8ca46894ad945
egbog <rysimpry@yahoo.com>
2024-04-04 23:53:40 -0700

    Update RepairMaxDurability.cs
    null conditional operator update
    remove ownership check for repair kit
    change targetItem null check
    simplify main loop (1 less if-else)
    return original code if no repairablecomponent

--- RepairMaxDurability.cs
+++ RepairMaxDurability.cs
@@ -37,6 +37,7 @@ namespace MaxDura
 	public static class Constants
 	{
 		public static bool isRepairable = false;
+		public static bool isOwned = false;
 	}
 
 	public class RepairMaxDurability : ModulePatch
@@ -66,83 +67,69 @@ namespace MaxDura
 			ItemView componentInParent;
 			ItemContextAbstractClass targetItemContextAbstractClass;
 			Item targetItem = null;
+			RepairableComponent repairableComponent = null;
 
 			// make sure item is dragged onto another item, prevent nullpointers
 			if (eventData.pointerEnter != null)
 			{
 				componentInParent = eventData.pointerEnter.GetComponentInParent<ItemView>();
-				targetItemContextAbstractClass = componentInParent.ItemContext;
-				targetItem = targetItemContextAbstractClass.Item;
+				targetItemContextAbstractClass = componentInParent?.ItemContext;
+				targetItem = targetItemContextAbstractClass?.Item;
+			}
 
-				// verify the item is owned by the player and not in a trader's assort
-				if (!CheckOwner(targetItem))
-				{
-					targetItem = null;
-					log.LogWarning("UNABLE TO REPAIR ITEMS IN TRADER'S INVENTORY!");
-				}
-
-				// verify we're not dragging a repair kit from a trader's assort
-				if (!CheckOwner(dragItemContext.Item))
-				{
-					dragItemContext = null;
-					log.LogWarning("UNABLE TO REPAIR USING ITEMS IN TRADER'S INVENTORY!");
-				}
-			} 
-
-			// only do work when our item is dragged AND dragged onto another item
-			// make sure the item being dragged is the repair kit
-			if (dragItemContext != null && targetItem != null && CheckName(dragItemContext.Item))
+			if (targetItem != null)
 			{
 				// make sure it's an item that can actually be repaired ie. weapon
 				// must contain a RepairableComponent
-				if (targetItem.TryGetItemComponent<RepairableComponent>(out RepairableComponent repairableComponent))
+				targetItem.TryGetItemComponent<RepairableComponent>(out repairableComponent);
+				// check target item ownership
+				Constants.isOwned = CheckOwner(targetItem);
+			}
+
+			// only do work when our item is dragged AND dragged onto another item
+			// make sure the item being dragged is the repair kit
+			if (Constants.isOwned && CheckName(dragItemContext.Item) && repairableComponent != null)
+			{
+				// check if the durability is below 100
+				// set isRepairable to true if it is below 100
+				Constants.isRepairable = repairableComponent.MaxDurability != 100;
+
+				if (Constants.isRepairable)
 				{
-					// check if the durability is below 100
-					// set isRepairable to true if it is below 100
-					Constants.isRepairable = repairableComponent.MaxDurability != 100;
-
-					if (Constants.isRepairable)
+					ItemInfo info = new() // setup json to send to server
 					{
-						ItemInfo info = new() // setup json to send to server
-						{
-							name = targetItem.LocalizedName(),
-							id = targetItem.Id,
-							durability = repairableComponent.Durability,
-							maxDurability = repairableComponent.MaxDurability,
-							repairKitId = dragItemContext.Item.Id
-						};
+						name = targetItem.LocalizedName(),
+						id = targetItem.Id,
+						durability = repairableComponent.Durability,
+						maxDurability = repairableComponent.MaxDurability,
+						repairKitId = dragItemContext.Item.Id
+					};
 
-                        ParseProfile prof = new(Post("/MaxDura/CheckDragged", JsonConvert.SerializeObject(info))); // instantiate our profile parsing class
-						bool updated = prof.UpdateValues(repairableComponent, dragItemContext.Item); // instantiate our profile parsing class
-						string status = updated ? "REPAIR SUCCESSFUL" : "REPAIR FAILED: JSON ERROR";
+                    ParseProfile prof = new(Post("/MaxDura/CheckDragged", JsonConvert.SerializeObject(info))); // instantiate our profile parsing class
+					bool updated = prof.UpdateValues(repairableComponent, dragItemContext.Item); // instantiate our profile parsing class
+					string status = updated ? "REPAIR SUCCESSFUL" : "REPAIR FAILED: JSON ERROR";
 
-						if (updated) // success
-						{
-							// sound and notification
-							Singleton<GUISounds>.Instance.PlayUISound(EUISoundType.RepairComplete);
-							NotificationManagerClass.DisplayMessageNotification(string.Format("{0} {1:F1}", "Item successfully repaired to".Localized(null),
-								repairableComponent.MaxDurability), ENotificationDurationType.Default, ENotificationIconType.Default, null);
-							log.LogInfo(status);
-						}
-						else // failure for some reason
-						{
-							Singleton<GUISounds>.Instance.PlayUISound(EUISoundType.ErrorMessage);
-							NotificationManagerClass.DisplayMessageNotification("Repair failed: JSON error", ENotificationDurationType.Default, ENotificationIconType.Alert, null);
-							log.LogError(status);
-						}
-                    }
-					else // item already at 100 max durability
+					if (updated) // success
+					{
+						// sound and notification
+						Singleton<GUISounds>.Instance.PlayUISound(EUISoundType.RepairComplete);
+						NotificationManagerClass.DisplayMessageNotification(string.Format("{0} {1:F1}", "Item successfully repaired to".Localized(null),
+							repairableComponent.MaxDurability), ENotificationDurationType.Default, ENotificationIconType.Default, null);
+						log.LogInfo(status);
+					}
+					else // failure for some reason
 					{
 						Singleton<GUISounds>.Instance.PlayUISound(EUISoundType.ErrorMessage);
-						NotificationManagerClass.DisplayMessageNotification("Item already at maximum durability", ENotificationDurationType.Default, ENotificationIconType.Alert, null);
-						log.LogInfo("NO REPAIR NECESSARY");
-						dragItemContext.DragCancelled();
-                    }
+						NotificationManagerClass.DisplayMessageNotification("Repair failed: JSON error", ENotificationDurationType.Default, ENotificationIconType.Alert, null);
+						log.LogError(status);
+					}
                 }
-				else // targetItem is something other than a gun
+				else // item already at 100 max durability
 				{
-                    log.LogWarning("REPAIR FAILED: NO REPAIRABLECOMPONENT DETECTED");
-                    dragItemContext.DragCancelled();
+					Singleton<GUISounds>.Instance.PlayUISound(EUISoundType.ErrorMessage);
+					NotificationManagerClass.DisplayMessageNotification("Item already at maximum durability", ENotificationDurationType.Default, ENotificationIconType.Alert, null);
+					log.LogInfo("NO REPAIR NECESSARY");
+					dragItemContext.DragCancelled();
                 }
 				// whether repair fails or completes
 				// stop original code from executing
