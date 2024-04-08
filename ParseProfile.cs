34cbd4e7eeed8118bf21171744c24e61a8894a51
egbog <rysimpry@yahoo.com>
2024-04-07 22:46:22 -0700

    refactor and include updating current durability
    remove Constants class
    reduce size of ItemInfo struct
    reduce amount of else statements to streamline code

--- ParseProfile.cs
+++ ParseProfile.cs
@@ -17,7 +17,7 @@ namespace MaxDura
     public class ParseProfile
     {
 		private readonly string idSophie, idBenji;
-		private readonly int maxD, repR;
+		private readonly int newD, maxD, repR;
 		private readonly ManualLogSource log;
 
 		public ParseProfile(JObject json)
@@ -32,16 +32,18 @@ namespace MaxDura
             {
                 idSophie = (string)sophie.SelectToken("_id");
                 idBenji = (string)benji.SelectToken("_id");
-                maxD = (int)sophie.SelectToken("upd.Repairable.MaxDurability");
-                repR = (int)benji.SelectToken("upd.RepairKit.Resource");
+				newD = (int)sophie.SelectToken("upd.Repairable.Durability");
+				maxD = (int)sophie.SelectToken("upd.Repairable.MaxDurability");
+				repR = (int)benji.SelectToken("upd.RepairKit.Resource");
             }
         }
 
 		public bool UpdateValues(RepairableComponent targetItemRC, Item repairKit)
 		{
-			// set weapon max durability
+			// set weapon durability
 			if (targetItemRC.Item.Id == idSophie)
 			{
+				targetItemRC.Durability = newD;
 				targetItemRC.MaxDurability = maxD;
 				targetItemRC.Item.UpdateAttributes();
 				//this.log.LogInfo(item.LocalizedName() + " REPAIRED TO: " + repairableComponent.MaxDurability);
--- RepairMaxDurability.cs
+++ RepairMaxDurability.cs
@@ -25,23 +25,13 @@ using Newtonsoft.Json.Linq;
 
 namespace MaxDura
 {
-	public struct ItemInfo
-	{
-		public string name;
-		public string id;
-		public float durability;
-		public float maxDurability;
-		public string repairKitId;
-	}
-
-	public static class Constants
-	{
-		public static bool isRepairable = false;
-		public static bool isOwned = false;
-	}
-
 	public class RepairMaxDurability : ModulePatch
 	{
+		public struct ItemInfo
+		{
+			public string itemId;
+			public string repairKitId;
+		}
 		public static JObject Post(string url, string data)
 		{
 			return JObject.Parse(RequestHandler.PostJson(url, data));
@@ -53,6 +43,12 @@ namespace MaxDura
 		public static bool CheckName(Item item)
 		{
 			return item.LocalizedName() == "Spare Firearm Parts";
+		}
+		public static bool CheckDurabilityIsWithinRange(RepairableComponent repairableComponent1)
+		{
+			string dur = repairableComponent1.Durability.ToString("f2");
+			string maxDur = repairableComponent1.MaxDurability.ToString("f2");
+			return dur == maxDur;
 		}
 		protected override MethodBase GetTargetMethod()
 		{
@@ -68,6 +64,7 @@ namespace MaxDura
 			ItemContextAbstractClass targetItemContextAbstractClass;
 			Item targetItem = null;
 			RepairableComponent repairableComponent = null;
+			bool start = false;
 
 			// make sure item is dragged onto another item, prevent nullpointers
 			if (eventData.pointerEnter != null)
@@ -77,60 +74,71 @@ namespace MaxDura
 				targetItem = targetItemContextAbstractClass?.Item;
 			}
 
+			// check if we dragged onto an item
 			if (targetItem != null)
 			{
 				// make sure it's an item that can actually be repaired ie. weapon
 				// must contain a RepairableComponent
 				targetItem.TryGetItemComponent<RepairableComponent>(out repairableComponent);
+
 				// check target item ownership
-				Constants.isOwned = CheckOwner(targetItem);
+				start = CheckName(dragItemContext.Item) && CheckOwner(targetItem) && repairableComponent != null;
 			}
 
 			// only do work when our item is dragged AND dragged onto another item
 			// make sure the item being dragged is the repair kit
-			if (Constants.isOwned && CheckName(dragItemContext.Item) && repairableComponent != null)
+			if (start)
 			{
 				// check if the durability is below 100
 				// set isRepairable to true if it is below 100
-				Constants.isRepairable = repairableComponent.MaxDurability != 100;
+				#pragma warning disable S1244 // Floating point numbers should not be tested for equality
+				bool isRepairable = repairableComponent.MaxDurability != 100f;
+				#pragma warning restore S1244 // Floating point numbers should not be tested for equality
+				bool isWithinRange = CheckDurabilityIsWithinRange(repairableComponent);
 
-				if (Constants.isRepairable)
-				{
-					ItemInfo info = new() // setup json to send to server
-					{
-						name = targetItem.LocalizedName(),
-						id = targetItem.Id,
-						durability = repairableComponent.Durability,
-						maxDurability = repairableComponent.MaxDurability,
-						repairKitId = dragItemContext.Item.Id
-					};
-
-                    ParseProfile prof = new(Post("/MaxDura/CheckDragged", JsonConvert.SerializeObject(info))); // instantiate our profile parsing class
-					bool updated = prof.UpdateValues(repairableComponent, dragItemContext.Item); // instantiate our profile parsing class
-					string status = updated ? "REPAIR SUCCESSFUL" : "REPAIR FAILED: JSON ERROR";
-
-					if (updated) // success
-					{
-						// sound and notification
-						Singleton<GUISounds>.Instance.PlayUISound(EUISoundType.RepairComplete);
-						NotificationManagerClass.DisplayMessageNotification(string.Format("{0} {1:F1}", "Item successfully repaired to".Localized(null),
-							repairableComponent.MaxDurability), ENotificationDurationType.Default, ENotificationIconType.Default, null);
-						log.LogInfo(status);
-					}
-					else // failure for some reason
-					{
-						Singleton<GUISounds>.Instance.PlayUISound(EUISoundType.ErrorMessage);
-						NotificationManagerClass.DisplayMessageNotification("Repair failed: JSON error", ENotificationDurationType.Default, ENotificationIconType.Alert, null);
-						log.LogError(status);
-					}
-                }
-				else // item already at 100 max durability
+				if(!isRepairable) // item already at 100 max durability
 				{
 					Singleton<GUISounds>.Instance.PlayUISound(EUISoundType.ErrorMessage);
 					NotificationManagerClass.DisplayMessageNotification("Item already at maximum durability", ENotificationDurationType.Default, ENotificationIconType.Alert, null);
-					log.LogInfo("NO REPAIR NECESSARY");
 					dragItemContext.DragCancelled();
-                }
+					//log.LogInfo("NO REPAIR NECESSARY");
+					return false;
+				}
+
+				if (!isWithinRange) // current durability is not at the maximum it can be at the moment
+				{
+					Singleton<GUISounds>.Instance.PlayUISound(EUISoundType.ErrorMessage);
+					NotificationManagerClass.DisplayMessageNotification("Item not clean enough to install new parts", ENotificationDurationType.Default, ENotificationIconType.Alert, null);
+					dragItemContext.DragCancelled();
+					return false;
+				}
+
+				// if code runs to here, then we satisfied all conditions to start the repair process
+
+				ItemInfo info = new() // setup json to send to server
+				{
+					itemId = targetItem.Id,
+					repairKitId = dragItemContext.Item.Id
+				};
+
+                ParseProfile prof = new(Post("/MaxDura/CheckDragged", JsonConvert.SerializeObject(info))); // instantiate our profile parsing class
+				bool updated = prof.UpdateValues(repairableComponent, dragItemContext.Item); // set durability and repair kit resource
+				string status = updated ? "REPAIR SUCCESSFUL" : "REPAIR FAILED: JSON ERROR";
+
+				if (updated) // success
+				{
+					// sound and notification
+					Singleton<GUISounds>.Instance.PlayUISound(EUISoundType.RepairComplete);
+					NotificationManagerClass.DisplayMessageNotification(string.Format("{0} {1:F1}", "Item successfully repaired to".Localized(null),
+						repairableComponent.MaxDurability), ENotificationDurationType.Default, ENotificationIconType.Default, null);
+					log.LogInfo(status);
+				}
+				else // failure for some reason
+				{
+					Singleton<GUISounds>.Instance.PlayUISound(EUISoundType.ErrorMessage);
+					NotificationManagerClass.DisplayMessageNotification("Repair failed: JSON error", ENotificationDurationType.Default, ENotificationIconType.Alert, null);
+					log.LogError(status);
+				}
 				// whether repair fails or completes
 				// stop original code from executing
 				// in this case prevent repair window from opening
