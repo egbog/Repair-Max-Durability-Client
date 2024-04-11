ca188068e7b44ddb48d953b5173326be2f0cfcf7
egbog <rysimpry@yahoo.com>
2024-04-11 00:10:59 -0700

    copyright shiz

---
+++ AssemblyInfo.cs
@@ -0,0 +1,26 @@
+﻿using System.Reflection;
+
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("MaxDura")]
+[assembly: AssemblyDescription("MaxDura")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("egbog")]
+[assembly: AssemblyProduct("MaxDura")]
+[assembly: AssemblyCopyright("Copyright © egbog 2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Version information for an assembly consists of the following four values:
+//
+// Major Version
+// Minor Version
+// Build Number
+// Revision
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.1.0")]
+
