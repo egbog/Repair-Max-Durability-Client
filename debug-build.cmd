152db1b4d1b449ae65b03920461d566718b285a4
egbog <rysimpry@yahoo.com>
2024-11-27 19:18:12 -0700

    Update debug-build.cmd

--- debug-build.cmd
+++ debug-build.cmd
@@ -1,2 +1,2 @@
 dotnet build
-copy bin\Debug\net471\MaxDura.dll "E:\games\aki 3.9.0\BepInEx\plugins\MaxDura.dll"
+copy bin\Debug\net471\MaxDura.dll "E:\games\SPT 3.10 DEV\BepInEx\plugins\MaxDura.dll"
